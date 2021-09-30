312ff332dddc94bb3a8f118ca03e9467217934e7
jeison0894 <jeisson0894@gmail.com>
2021-09-30 16:25:32 -0500

    Config backend to do deploy with heroku

--- API/config/conexion.js
+++ API/config/conexion.js
@@ -1,19 +1,21 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-   host:'localhost',
-   user:'root',
-   password:'root',
+   host:'bzvrqimrhuvizlayduoi-mysql.services.clever-cloud.com',
+   user:'ueqltvghdsttpr5e',
+   password:'BTevwcrdtWUqVjUl4Go2',
    port:3306,
-   database:'movies'
+   database:'bzvrqimrhuvizlayduoi'
 })
 
-connection.connect((err)=>{
-   if (err) {
-      console.log('There is an error' + err)
-   } else {
-      console.log('Database connected.')
-   }
-})
+connection.connect((err) => {
+    if (err) {
+        console.log('ha ocurrido un error' + err)
+    } else {
+        console.log('La base de datos se coneecto exitosamente')
+    }
+});
+setInterval(function () {
+    connection.query('SELECT 1');
+}, process.env.PORT || 3000);
 
-
-module.exports=connection;
+module.exports = connection;
--- API/index.js
+++ API/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 
 // Si no existe el puerto con el operador corto circuito lo colocamos en el puerto 3000
-const port = (process.env.port || 3000)
+const port = ( process.env.PORT || 3000);
+const path = require('path');
 
 
 //Creamos la instancia de express guardada en una variable
@@ -18,10 +19,13 @@ app.use(express.json())
 //configuramos el puerto
 app.set('port',port)
 
-
+app.use( express.static('public'))
 //Pasamos las rutas
 app.use('/api',require('./routes'))
 
+app.get( '*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html'))
+});
 
 //Iniciamos express
 app.listen(app.get('port'),(err)=>{
